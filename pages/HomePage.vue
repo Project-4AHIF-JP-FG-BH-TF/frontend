1d6e3decdc1ec92daf710be26bcc22241779ab3f
Fabian <fabian.goetz06@gmail.com>
2023-12-20 11:44:25 +0100

    Reformatted with prettier

--- pages/HomePage.vue
+++ pages/HomePage.vue
@@ -1,39 +1,31 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-
   <div id="main">
-
     <aside id="sidebar-left">
       <div>
-        <img id="logo" src="assets/loggaroo_logo.svg" alt="Loggaroo Logo">
+        <img id="logo" src="assets/loggaroo_logo.svg" alt="Loggaroo Logo" />
       </div>
 
       <div id="actions">
-        <button class="button">Log-Dateien</button><br>
-        <button class="button">Log-Einträge</button><br>
-        <button class="button">Statistiken</button><br>
+        <button class="button">Log-Dateien</button>
+        <br />
+        <button class="button">Log-Einträge</button>
+        <br />
+        <button class="button">Statistiken</button>
+        <br />
       </div>
-
     </aside>
 
     <div id="main-view">
-
       <div>
         <h1>MAIN TABLE</h1>
       </div>
-
     </div>
-
   </div>
-
-
 </template>
 
 <style scoped lang="scss">
-
 * {
   font-size: large;
   color: white;
@@ -56,28 +48,25 @@
     justify-content: flex-start;
     align-items: center;
 
-    #logo{
+    #logo {
       height: 120px;
     }
   }
 }
 
-
 #main-view {
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100%
+  height: 100%;
 }
 
-.button{
+.button {
   border: gray 2px solid;
   background-color: gray;
   border-radius: 10px;
-  width:150px;
+  width: 150px;
   margin: 15px;
   padding: 5px;
 }
-
-
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-<!--    <NuxtWelcome/>-->
+    <!--    <NuxtWelcome/>-->
     <HomePage></HomePage>
   </div>
 </template>
