7dfe69de73472290b39c05abcbef96fffcea9280
Fabian <fabian.goetz06@gmail.com>
2024-01-08 14:31:18 +0100

    implemented a store for LogEntries formatted

--- components/TableView.vue
+++ components/TableView.vue
@@ -1,54 +1,11 @@
 <script setup lang="ts">
-const logs = ref([
-  {
-    date: "8.1.2024 9:30",
-    level: "ERROR",
-    ip: "192.168.0.1",
-    user_id: 123,
-    session_id: 456,
-    text: "Zusatz Kommentar ZusatzZusatz ZusatzZusatz",
-  },
-  {
-    date: "17.12.2023 23:59",
-    level: "INFO",
-    ip: "192.168.0.12",
-    user_id: 2,
-    session_id: 35,
-    text: "Zusatz-Text",
-  },
-  {
-    date: "7.1.2024 10:45",
-    level: "INFO",
-    ip: "192.168.0.45",
-    user_id: 1,
-    session_id: 2,
-    text: "Zusatz-Text",
-  },
-  {
-    date: "6.1.2024 10:45",
-    level: "INFO",
-    ip: "192.168.0.45",
-    user_id: 1,
-    session_id: 5,
-    text: "Zusatz-Text",
-  },
-  {
-    date: "5.1.2024 10:45",
-    level: "INFO",
-    ip: "192.168.0.45",
-    user_id: 1,
-    session_id: 65,
-    text: "Zusatz-Text",
-  },
-  {
-    date: "4.1.2024 10:45",
-    level: "ERROR",
-    ip: "192.168.0.45",
-    user_id: 1,
-    session_id: 12,
-    text: "Kommentar",
-  },
-]);
+import { useLogEntryStore } from "~/stores/logEntryStore";
+
+const entryStore = useLogEntryStore();
+
+const logs = ref(entryStore.getEntries);
+
+entryStore.loadEntries(0, 0);
 </script>
 
 <template>
@@ -73,23 +30,32 @@ const logs = ref([
         <span class="headerElement border-bottom flex-10">Text</span>
       </div>
       <!-- Liste an Log-Einträgen -->
-      <div class="list">
-        <div class="list-element" v-for="log in logs">
-          <span class="list-data border-right flex-4">{{ log.date }}</span>
-          <div class="list-data border-right flex-3" id="level-field">
-            <div v-if="log.level == 'ERROR'" class="log-type error">
+      <div id="list">
+        <div
+          v-for="log in logs"
+          :key="log.file_name + log.entry_nr.toString()"
+          class="list-element"
+        >
+          <span class="list-data border-right flex-4">{{
+            // toFormattedDate(log.creation_date)
+            log.creation_date.toLocaleString()
+          }}</span>
+          <div id="level-field" class="list-data border-right flex-3">
+            <div v-if="log.classification == 'error'" class="log-type error">
               <span>!</span>
             </div>
             <div v-else class="log-type info"></div>
           </div>
-          <span class="list-data border-right flex-4">{{ log.ip }}</span>
+          <span class="list-data border-right flex-4">{{
+            log.service_ip
+          }}</span>
           <span class="list-data border-right flex-3">{{ log.user_id }}</span>
           <span class="list-data border-right flex-3">{{
-            log.session_id
+            log.user_session_id
           }}</span>
-          <span class="list-data text-overflow-ellipsis flex-10">{{
-            log.text
-          }}</span>
+          <span class="list-data text-overflow-ellipsis flex-10"
+            >{{ log.java_class }} {{ log.content }}</span
+          >
         </div>
       </div>
     </div>
@@ -119,6 +85,7 @@ const logs = ref([
     height: 90%;
     background-color: #535353;
     border-radius: 10px;
+    overflow: hidden;
   }
 
   #tableHeader {
@@ -142,6 +109,23 @@ const logs = ref([
 
   .border-right {
     border-right: #2e2e2e solid 2px;
+  }
+
+  #list {
+    height: 90%;
+    overflow: scroll;
+    overflow-x: hidden;
+  }
+
+  /* Hide scrollbar for Chrome, Safari and Opera */
+  #list::-webkit-scrollbar {
+    display: none;
+  }
+
+  /* Hide scrollbar for IE, Edge and Firefox */
+  #list {
+    -ms-overflow-style: none; /* IE and Edge */
+    scrollbar-width: none; /* Firefox */
   }
 
   .list-element {
---
+++ stores/logEntryStore.ts
@@ -0,0 +1,133 @@
+import type { UUID } from "node:crypto";
+import { defineStore } from "pinia";
+import { generateUuid } from "vscode-languageclient/lib/common/utils/uuid";
+
+export interface LogEntry {
+  session_id: UUID;
+  file_name: string;
+  entry_nr: number;
+  creation_date: Date;
+  classification: "info" | "error";
+  service_ip: string;
+  user_id: string;
+  user_session_id: string;
+  java_class: string;
+  content: string;
+}
+
+interface State {
+  entries: LogEntry[];
+}
+
+interface Filters {}
+
+export const useLogEntryStore = defineStore("logEntries", {
+  state: (): State => ({
+    entries: [],
+  }),
+  getters: {
+    getEntries: (state): LogEntry[] => state.entries,
+  },
+  actions: {
+    addEntry(entry: LogEntry) {
+      this.entries.push(entry);
+    },
+    addEntries(logEntries: LogEntry[]) {
+      for (const logEntry of logEntries) {
+        this.addEntry(logEntry);
+      }
+    },
+    clearEntries() {
+      this.entries = [];
+    },
+    async loadEntries(
+      _start: number,
+      _count: number,
+      _desc: boolean = false,
+      _filters: Filters = {},
+    ) {
+      await this.clearEntries();
+
+      // to bypass lint
+
+      // TODO with backend
+      const testEntries: LogEntry[] = [
+        {
+          session_id: generateUuid() as UUID,
+          file_name: "",
+          entry_nr: 0,
+          creation_date: new Date(Date.parse("2024-01-02T12:34:23.1234+01:00")),
+          classification: "error",
+          service_ip: "192.168.0.1",
+          user_id: "123",
+          user_session_id: "456",
+          java_class: "",
+          content: "Zusatz Kommentar ZusatzZusatz ZusatzZusatz",
+        },
+        {
+          session_id: generateUuid() as UUID,
+          file_name: "",
+          entry_nr: 0,
+          creation_date: new Date(Date.parse("2024-01-01T10:34:23.1234+01:00")),
+          classification: "info",
+          service_ip: "192.168.0.45",
+          user_id: "1",
+          user_session_id: "123",
+          java_class: "",
+          content: "Zusatz Kommentar",
+        },
+        {
+          session_id: generateUuid() as UUID,
+          file_name: "",
+          entry_nr: 0,
+          creation_date: new Date(Date.parse("2024-01-01T12:00:23.1234+01:00")),
+          classification: "info",
+          service_ip: "192.168.0.1",
+          user_id: "123",
+          user_session_id: "456",
+          java_class: "",
+          content: "Zusatz Kommentar ZusatzZusatz ZusatzZusatz",
+        },
+        {
+          session_id: generateUuid() as UUID,
+          file_name: "",
+          entry_nr: 0,
+          creation_date: new Date(Date.parse("2024-01-02T12:34:23.1234+01:00")),
+          classification: "error",
+          service_ip: "192.168.0.1",
+          user_id: "123",
+          user_session_id: "456",
+          java_class: "",
+          content: "Zusatz Kommentar ZusatzZusatz ZusatzZusatz",
+        },
+        {
+          session_id: generateUuid() as UUID,
+          file_name: "",
+          entry_nr: 0,
+          creation_date: new Date(Date.parse("2024-01-02T12:34:23.1234+01:00")),
+          classification: "error",
+          service_ip: "192.168.0.1",
+          user_id: "123",
+          user_session_id: "456",
+          java_class: "",
+          content: "Zusatz Kommentar ZusatzZusatz ZusatzZusatz",
+        },
+        {
+          session_id: generateUuid() as UUID,
+          file_name: "",
+          entry_nr: 0,
+          creation_date: new Date(Date.parse("2024-01-02T12:34:23.1234+01:00")),
+          classification: "error",
+          service_ip: "192.168.0.1",
+          user_id: "123",
+          user_session_id: "456",
+          java_class: "",
+          content: "Zusatz Kommentar ZusatzZusatz ZusatzZusatz",
+        },
+      ];
+
+      this.addEntries(testEntries);
+      this.addEntries(testEntries);
+    },
+  },
+});
