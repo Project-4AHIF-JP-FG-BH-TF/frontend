887ff4f4a36d45abfebe7e211c01def80231b391
Jonas Pfeifer <jonaspfeifer@drei.at>
2024-01-08 10:47:48 +0100

    removed axios module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: {enabled: true},
-    modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", '@nuxtjs/axios'],
+    modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
     runtimeConfig: {
         public: {
             baseURL: process.env.BASE_URL || 'https://api.example.com/',
