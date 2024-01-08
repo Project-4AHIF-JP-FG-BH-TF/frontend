915e876972f2cfe06b88a8508234f308b9825616
Jonas Pfeifer <jonaspfeifer@drei.at>
2024-01-08 10:41:36 +0100

    added initial request and refresh

--- app.vue
+++ app.vue
@@ -1,4 +1,14 @@
-<script>
+<script setup>
+import {useSessionStore} from "~/stores/sessionStore.js";
+
+const sessionStore = useSessionStore();
+
+await callOnce(async () => {
+  await sessionStore.requestSession();
+  setInterval(() => {
+    sessionStore.refreshSession();
+  }, 1000 * 60)
+})
 
 </script>
 
