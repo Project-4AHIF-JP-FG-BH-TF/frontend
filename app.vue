4f71633526b8d4385da8c1af7ebbdab2101c7b7b
Jonas Pfeifer <jonaspfeifer@drei.at>
2024-01-08 10:25:04 +0100

    added session store

--- app.vue
+++ app.vue
@@ -1,3 +1,7 @@
+<script>
+
+</script>
+
 <template>
   <nuxt-layout>
     <nuxt-page />
---
+++ stores/sessionStore.ts
@@ -0,0 +1,46 @@
+import {defineStore} from "pinia"
+import {useAsyncData} from "nuxt/app";
+import type {UUID} from "node:crypto";
+
+interface State {
+    sessionID: UUID | null
+}
+
+export const useSessionStore = defineStore("session", {
+    state: (): State => ({
+        sessionID: null
+    }),
+    getters: {
+        getSessionID: (state) => state.sessionID,
+    },
+    actions: {
+        async requestSession() {
+            this.sessionID = await fetchNewSession();
+        },
+        async refreshSession() {
+            if (this.sessionID != null) {
+                await fetchRefreshSession(this.sessionID);
+            }
+        }
+    },
+})
+
+async function fetchNewSession(): Promise<UUID | null> {
+    const {data, error} = await useAsyncData(async () =>
+        await useFetch("/api/session/", {
+            method: "GET",
+        })
+    );
+
+    if (error.value != null) return null;
+    return data.value!.data.value as UUID;
+}
+
+async function fetchRefreshSession(sessionID: UUID): Promise<void> {
+    await useFetch(`/api/session/${sessionID}`, {
+        method: "POST"
+    })
+}
+
+
+
