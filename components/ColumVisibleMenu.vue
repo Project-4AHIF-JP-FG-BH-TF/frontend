830456f23ee77cce79fb861e81729fc228fcc3ca
benni <benni.huff@gmail.com>
2024-02-21 11:43:21 +0100

    now at least one column must be visible

--- components/ColumVisibleMenu.vue
+++ components/ColumVisibleMenu.vue
@@ -4,25 +4,38 @@ import type { Columns } from "~/types/LogEntry";
 
 const model = defineModel<Columns>();
 
+const countActive = ref(6);
+
 function change(type: Types) {
   switch (type) {
     case Types.DATE:
       model.value!.date = !model.value!.date;
+      model.value?.date ? countActive.value++ : countActive.value--;
+
       break;
     case Types.LEVEL:
       model.value!.level = !model.value!.level;
+      model.value?.level ? countActive.value++ : countActive.value--;
+
       break;
     case Types.IP_ADDRESS:
       model.value!.ip = !model.value!.ip;
+      model.value?.ip ? countActive.value++ : countActive.value--;
+
       break;
     case Types.USER_ID:
       model.value!.user = !model.value!.user;
+      model.value?.user ? countActive.value++ : countActive.value--;
+
       break;
     case Types.SESSION_ID:
       model.value!.session = !model.value!.session;
+      model.value?.session ? countActive.value++ : countActive.value--;
+
       break;
     case Types.TEXT:
       model.value!.text = !model.value!.text;
+      model.value?.text ? countActive.value++ : countActive.value--;
   }
 }
 </script>
@@ -37,6 +50,7 @@ function change(type: Types) {
         type="checkbox"
         name="date"
         :checked="model?.date"
+        :disabled="countActive === 1 && model?.date"
         @change="change(Types.DATE)"
       />
       <label for="date">Datum</label>
@@ -48,6 +62,7 @@ function change(type: Types) {
         type="checkbox"
         name="level"
         :checked="model?.level"
+        :disabled="countActive === 1 && model?.level"
         @change="change(Types.LEVEL)"
       />
       <label for="level">Level</label>
@@ -59,6 +74,7 @@ function change(type: Types) {
         type="checkbox"
         name="ipaddress"
         :checked="model?.ip"
+        :disabled="countActive === 1 && model?.ip"
         @change="change(Types.IP_ADDRESS)"
       />
       <label for="IpAddress">IP-Adresse</label>
@@ -70,6 +86,7 @@ function change(type: Types) {
         type="checkbox"
         name="userID"
         :checked="model?.user"
+        :disabled="countActive === 1 && model?.user"
         @change="change(Types.USER_ID)"
       />
       <label for="userID">Nutzer-ID</label>
@@ -81,6 +98,7 @@ function change(type: Types) {
         type="checkbox"
         name="sessionID"
         :checked="model?.session"
+        :disabled="countActive === 1 && model?.session"
         @change="change(Types.SESSION_ID)"
       />
       <label for="sessionID">Sitzungs-ID</label>
@@ -92,6 +110,7 @@ function change(type: Types) {
         type="checkbox"
         name="text"
         :checked="model?.text"
+        :disabled="countActive === 1 && model?.text"
         @change="change(Types.TEXT)"
       />
       <label for="text">Text</label>
