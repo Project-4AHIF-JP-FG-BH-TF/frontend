4a2c51261da0dede2b4c896c07d4212db4e59552
Fabian <fabian.goetz06@gmail.com>
2024-04-03 11:58:27 +0200

    added empty links to charts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const statisticsExpanded = ref(false);
+</script>
 
 <template>
   <div id="base">
@@ -8,9 +10,25 @@
       </div>
 
       <div id="actions">
-        <nuxt-link to="/logfiles">Log-Dateien</nuxt-link>
-        <nuxt-link to="/">Log-Einträge</nuxt-link>
-        <nuxt-link to="/statistics">Statistiken</nuxt-link>
+        <nuxt-link to="/logfiles" class="links">Log-Dateien</nuxt-link>
+        <nuxt-link to="/" class="links">Log-Einträge</nuxt-link>
+        <!--        <nuxt-link to="/statistics">Statistiken</nuxt-link>-->
+
+        <div id="chartSelection">
+          <span
+            id="statistics"
+            :class="{ noBorderRadiusBottom: statisticsExpanded }"
+            @click="statisticsExpanded = !statisticsExpanded"
+            >Statistiken</span
+          >
+          <div v-if="statisticsExpanded" id="chartSelectorsContainer">
+            <nuxt-link class="chartSelectors">BarChart</nuxt-link>
+            <nuxt-link class="chartSelectors">PieChart</nuxt-link>
+            <nuxt-link class="chartSelectors bottomSelector"
+              >LineChart
+            </nuxt-link>
+          </div>
+        </div>
       </div>
     </aside>
     <main>
@@ -54,7 +72,8 @@
 
       gap: var(--gap);
 
-      a {
+      .links,
+      #statistics {
         background-color: var(--base-background);
         border-radius: 10px;
         width: calc(var(--side-width) - var(--gap) * 2);
@@ -72,6 +91,48 @@
         &:active {
           background-color: var(--highlight);
           scale: 1.02;
+        }
+      }
+
+      #chartSelection {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+
+        .noBorderRadiusBottom {
+          border-bottom-right-radius: 0;
+          border-bottom-left-radius: 0;
+        }
+
+        #chartSelectorsContainer {
+          display: flex;
+          flex-direction: column;
+
+          .chartSelectors {
+            background-color: var(--base-background);
+            //width: calc(var(--side-width) - var(--gap) * 2);
+            //width: 100%;
+            padding: 5px;
+
+            font-size: large;
+            color: white;
+
+            transition: 0.1s ease-in-out;
+
+            &:hover {
+              background-color: var(--highlight);
+            }
+
+            &:active {
+              background-color: var(--highlight);
+              scale: 1.02;
+            }
+          }
+
+          .bottomSelector {
+            border-bottom-left-radius: 10px;
+            border-bottom-right-radius: 10px;
+          }
         }
       }
     }
